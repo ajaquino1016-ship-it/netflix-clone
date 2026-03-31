e6c6463658f9c1c730b4dd0ff586dc42361de64a
Liqour-ice <ajaquino1016@gmail.com>
2026-03-31 19:19:50 +0800

    adding netflix trailer

--- netflix/src/Row.js
+++ netflix/src/Row.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
-
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) { 
+// Added isLargeRow as a prop
+function Row({ title, fetchUrl, isLargeRow }) { 
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
@@ -23,9 +24,12 @@ function Row({ title, fetchUrl }) {
                 {movies && movies.map((movie) => (
                     <img
                         key={movie.id}
-                        className="row__poster"
                        
-                        src={`${base_url}${movie.poster_path}`}
+                        className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                    
+                        src={`${base_url}${
+                            isLargeRow ? movie.poster_path : movie.backdrop_path
+                        }`}
                         alt={movie.name}
                     />
                 ))}
